20bc97bba0e87770d00d91786349a7c1046d2c4f
Ciel (Xi) <122921783+CielZ001@users.noreply.github.com>
2023-06-27 21:29:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ import openai
 openai.api_key = st.secrets['openai_api_key']
 
 
-# This function uses the OpenAI Completion API to generate a
-# response based on the given prompt. The temperature parameter controls
-# the randomness of the generated response. A higher temperature will result
-# in more random responses,
-# while a lower temperature will result in more predictable responses.
+# A function to generate a response from GPT-3.5
 def generate_response(prompt):
     completions = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="tgpt-3.5-turbo",
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=256,
         n=1,
         stop=None,
         temperature=0,
@@ -24,15 +20,19 @@ def generate_response(prompt):
     return message
 
 
+# Site title
 st.title("🤖🔬 ChatBot for Learning Sciences Research")
 
+# Initialize output session state
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
 
+# Initialize input session state
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
 
+# Get user input
 def get_text():
     input_text = st.text_input("You: ", "Hello, how are you?", key="input")
     return input_text
@@ -40,11 +40,13 @@ def get_text():
 
 user_input = get_text()
 
+# Generate response
 if user_input:
     output = generate_response(user_input)
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
 
+# Display conversation
 if st.session_state['generated']:
 
     for i in range(len(st.session_state['generated']) - 1, -1, -1):
