4bad2d6de80b1c2e8dd4afa6187f44ec8b525040
Ciel (Xi) <122921783+CielZ001@users.noreply.github.com>
2023-07-07 19:16:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import streamlit as st
 from streamlit_chat import message
-import openai
-import os
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
 from langchain.vectorstores import Pinecone
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.llms import OpenAI
+from langchain.chains import ConversationalRetrievalChain
+from utils import *
+from langchain import PromptTemplate
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
-from langchain.memory import ConversationBufferMemory
-from langchain.llms import OpenAI
-from langchain import PromptTemplate
+import openai
+import os
+import streamlit as st
 
 openai.api_key = st.secrets['openai_api_key']
 
@@ -22,9 +24,10 @@ embed = OpenAIEmbeddings(
 
 index_name = 'cscl-langchain-retrieval-augmentation'
 
-PINECONE_API_KEY = '92e48ab1-885c-4c59-bf34-b16ad122e2c7'
-
-PINECONE_ENVIRONMENT = 'asia-southeast1-gcp-free'
+# find API key in console at app.pinecone.io
+PINECONE_API_KEY = 'a62589b4-c4d2-4f56-8812-342f7ac869f7'
+# find ENV (cloud region) next to API key in console
+PINECONE_ENVIRONMENT = 'us-west4-gcp-free'
 
 pinecone.init(
     api_key=PINECONE_API_KEY,
@@ -33,174 +36,102 @@ pinecone.init(
 
 text_field = "text"
 
+# switch back to normal index for langchain
 index = pinecone.Index(index_name)
 
 vectorstore = Pinecone(
     index, embed.embed_query, text_field
 )
 
-llm = ChatOpenAI(
-    openai_api_key=openai.api_key,
-    model_name='gpt-3.5-turbo',
-    temperature=0.0
-)
+memory = ConversationBufferWindowMemory(k=3, memory_key='chat_history', return_messages=True, output_key='answer')
 
-template = """Given the following extracted parts of a long document (delimited by <sm></sm>) and the chat history (delimited by <hs></hs>), create a final answer to the question with references ("SOURCES").
-If you don't know the answer, just say that you don't know. Don't try to make up an answer.
-ALWAYS return a "SOURCES" part in your answer.
 
-QUESTION: Which state/country's law governs the interpretation of the contract?
-=========
-<hs>
-Human: Who is the contract belongs to?
-AI: The contract belongs to a English company. SOURCES: 27-pl
-</hs>
-=========
-<sm>
-Content: This Agreement is governed by English law and the parties submit to the exclusive jurisdiction of the English courts in  relation to any dispute (contractual or non-contractual) concerning this Agreement save that either party may apply to any court for an  injunction or other relief to protect its Intellectual Property Rights.
-Source: 28-pl
-Content: No Waiver. Failure or delay in exercising any right or remedy under this Agreement shall not constitute a waiver of such (or any other)  right or remedy.\n\n11.7 Severability. The invalidity, illegality or unenforceability of any term (or part of a term) of this Agreement shall not affect the continuation  in force of the remainder of the term (if any) and this Agreement.\n\n11.8 No Agency. Except as expressly stated otherwise, nothing in this Agreement shall create an agency, partnership or joint venture of any  kind between the parties.\n\n11.9 No Third-Party Beneficiaries.
-Source: 30-pl
-Content: (b) if Google believes, in good faith, that the Distributor has violated or caused Google to violate any Anti-Bribery Laws (as  defined in Clause 8.5) or that such a violation is reasonably likely to occur,
-Source: 4-pl
-</sm>
-=========
-FINAL ANSWER: This Agreement is governed by English law.
-SOURCES: 28-pl
+def print_answer_citations_sources(result):
+    output_answer = ""
 
-QUESTION: What did the president say about Michael Jackson?
-=========
-<hs>
-Human: Who is the contract belongs to?
-AI: The contract belongs to a English company. SOURCES: 27-pl
-Human: Which state/country's law governs the interpretation of the contract?
-AI: TThis Agreement is governed by English law. SOURCES: 28-pl
-</hs>
-=========
-<sm>
-Content: Madam Speaker, Madam Vice President, our First Lady and Second Gentleman. Members of Congress and the Cabinet. Justices of the Supreme Court. My fellow Americans.  \n\nLast year COVID-19 kept us apart. This year we are finally together again. \n\nTonight, we meet as Democrats Republicans and Independents. But most importantly as Americans. \n\nWith a duty to one another to the American people to the Constitution. \n\nAnd with an unwavering resolve that freedom will always triumph over tyranny. \n\nSix days ago, Russia’s Vladimir Putin sought to shake the foundations of the free world thinking he could make it bend to his menacing ways. But he badly miscalculated. \n\nHe thought he could roll into Ukraine and the world would roll over. Instead he met a wall of strength he never imagined. \n\nHe met the Ukrainian people. \n\nFrom President Zelenskyy to every Ukrainian, their fearlessness, their courage, their determination, inspires the world. \n\nGroups of citizens blocking tanks with their bodies. Everyone from students to retirees teachers turned soldiers defending their homeland.
-Source: 0-pl
-Content: And we won’t stop. \n\nWe have lost so much to COVID-19. Time with one another. And worst of all, so much loss of life. \n\nLet’s use this moment to reset. Let’s stop looking at COVID-19 as a partisan dividing line and see it for what it is: A God-awful disease.  \n\nLet’s stop seeing each other as enemies, and start seeing each other for who we really are: Fellow Americans.  \n\nWe can’t change how divided we’ve been. But we can change how we move forward—on COVID-19 and other issues we must face together. \n\nI recently visited the New York City Police Department days after the funerals of Officer Wilbert Mora and his partner, Officer Jason Rivera. \n\nThey were responding to a 9-1-1 call when a man shot and killed them with a stolen gun. \n\nOfficer Mora was 27 years old. \n\nOfficer Rivera was 22. \n\nBoth Dominican Americans who’d grown up on the same streets they later chose to patrol as police officers. \n\nI spoke with their families and told them that we are forever in debt for their sacrifice, and we will carry on their mission to restore the trust and safety every community deserves.
-Source: 24-pl
-Content: And a proud Ukrainian people, who have known 30 years  of independence, have repeatedly shown that they will not tolerate anyone who tries to take their country backwards.  \n\nTo all Americans, I will be honest with you, as I’ve always promised. A Russian dictator, invading a foreign country, has costs around the world. \n\nAnd I’m taking robust action to make sure the pain of our sanctions  is targeted at Russia’s economy. And I will use every tool at our disposal to protect American businesses and consumers. \n\nTonight, I can announce that the United States has worked with 30 other countries to release 60 Million barrels of oil from reserves around the world.  \n\nAmerica will lead that effort, releasing 30 Million barrels from our own Strategic Petroleum Reserve. And we stand ready to do more if necessary, unified with our allies.  \n\nThese steps will help blunt gas prices here at home. And I know the news about what’s happening can seem alarming. \n\nBut I want you to know that we are going to be okay.
-Source: 5-pl
-Content: More support for patients and families. \n\nTo get there, I call on Congress to fund ARPA-H, the Advanced Research Projects Agency for Health. \n\nIt’s based on DARPA—the Defense Department project that led to the Internet, GPS, and so much more.  \n\nARPA-H will have a singular purpose—to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more. \n\nA unity agenda for the nation. \n\nWe can do this. \n\nMy fellow Americans—tonight , we have gathered in a sacred space—the citadel of our democracy. \n\nIn this Capitol, generation after generation, Americans have debated great questions amid great strife, and have done great things. \n\nWe have fought for freedom, expanded liberty, defeated totalitarianism and terror. \n\nAnd built the strongest, freest, and most prosperous nation the world has ever known. \n\nNow is the hour. \n\nOur moment of responsibility. \n\nOur test of resolve and conscience, of history itself. \n\nIt is in this moment that our character is formed. Our purpose is found. Our future is forged. \n\nWell I know this nation.
-Source: 34-pl
-</sm>
-=========
-FINAL ANSWER: The president did not mention Michael Jackson.
-SOURCES:
-
-QUESTION: {question}
-=========
-{history}
-=========
-{summaries}
-=========
-FINAL ANSWER:"""
-
-prompt = PromptTemplate(
-    input_variables=["summaries", "history", "question"],
-    template=template,
-)
-
-# def get_answer_citations_sources(result):
-#     answer = result['answer']
-#     unique_citations = {}
-
-#     for doc in result['source_documents']:
-#         citation = doc.metadata.get('citation')
-#         source = doc.metadata.get('source')
-#         if citation:
-#             unique_citations[citation] = source
-
-#     citations_sources = []
-#     for citation, source in unique_citations.items():
-#         citations_sources.append({
-#             'citation': citation,
-#             'source': source
-#         })
-
-#     return {
-#         'answer': answer,
-#         'citations_sources': citations_sources
-#     }
-
-def get_answer_citations_sources(result):
-    answer = result['answer']
-    unique_citations = {}
+    # Store the answer
+    output_answer += result['answer'] + "\n\n"
 
     # Extract the unique citations and their corresponding sources
+    unique_citations = {}
     for doc in result['source_documents']:
         citation = doc.metadata.get('citation')
         source = doc.metadata.get('source')
         if citation:
             unique_citations[citation] = source
 
-    # Create a string to store the answer, citations, and sources
-    result_string = ""
-
-    # Append the answer to the result string
-    result_string += answer + "\n\n"
-
-    # Append the unique citations and their corresponding sources to the result string
+    # Store the unique citations and their corresponding sources
     for citation, source in unique_citations.items():
-        result_string += "- Citation: " + citation + "\n"
-        result_string += "  Source: " + source + "\n\n"
+        output_answer += "- Citation: " + citation + "\n"
+        output_answer += "  Source: " + source + "\n\n"
 
-    return result_string
-
-
-def generate_response(prompt_input):
-    qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=vectorstore.as_retriever(),
-        chain_type_kwargs={
-            "verbose": True,
-            "prompt": prompt,
-            "memory": ConversationBufferMemory(input_key='question', memory_key='history')
-        },
-        return_source_documents=True
-    )
-
-    res = qa_with_sources(prompt_input)
-    # memo = qa_with_sources.combine_documents_chain.memory
-
-    message_output = get_answer_citations_sources(res)
-    # return str(memo)
-    return message_output
+    return output_answer
 
 
 # Site title
 st.title("🤖🔬 ChatBot for Learning Sciences Research")
 
 # Initialize output session state
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["How can I assist you?"]
 
 # Initialize input session state
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
 
+if 'buffer_memory' not in st.session_state:
+    st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
+
+
+qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0, openai_api_key=openai.api_key),
+                                           vectorstore.as_retriever(), memory=st.session_state.buffer_memory,
+                                           verbose=True,
+                                           return_source_documents=True)
+# container for chat history
+response_container = st.container()
+# container for text box
+textcontainer = st.container()
+
+with textcontainer:
+    query = st.text_input("Query: ", key="input")
+    if query:
+        with st.spinner("typing..."):
+            conversation_string = get_conversation_string()
+            # st.code(conversation_string)
+            refined_query = query_refiner(conversation_string, query)
+            st.subheader("Refined Query:")
+            st.write(refined_query)
+            context = find_match(refined_query)
+            # print(context)
+            res = qa(f"Context:\n {context} \n\n Query:\n{query}")
+            response = print_answer_citations_sources(res)
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response)
+with response_container:
+    if st.session_state['responses']:
+
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i], key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
 
 # Get user input
-def get_text():
-    input_text = st.text_input("You: ", "what is collaborative learning?", key="question")
-    return input_text
-
-
-user_input = get_text()
+# def get_text():
+#     input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+#     return input_text
+#
+#
+# user_input = get_text()
 
 # Generate response
-if user_input:
-    output = generate_response(user_input)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
+# if user_input:
+#     output = generate_response(user_input)
+#     st.session_state.past.append(user_input)
+#     st.session_state.generated.append(output)
 
 # Display conversation
-if st.session_state['generated']:
-
-    for i in range(len(st.session_state['generated']) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+# if st.session_state['generated']:
+#
+#     for i in range(len(st.session_state['generated']) - 1, -1, -1):
+#         message(st.session_state["generated"][i], key=str(i))
+#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
