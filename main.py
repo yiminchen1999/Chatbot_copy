8df818e6487e3c359aa3b62a5dd0b38e0f344b41
Ciel (Xi) <122921783+CielZ001@users.noreply.github.com>
2023-07-16 17:19:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import openai
 import os
 import streamlit as st
 
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = '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'
 # openai.api_key = OPENAI_API_KEY
 pc_api_key = "af46e200-9246-45a0-bc1d-3cdc544b9d2b"
 pc_env = "asia-southeast1-gcp-free"
