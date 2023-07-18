0c5e411cee548cc2ea23059b52ae996acabf5252
Ciel (Xi) <122921783+CielZ001@users.noreply.github.com>
2023-07-18 19:17:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import openai
 import os
 import streamlit as st
 
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, you should say that 'I've searched my database, but I couldn't locate the exact information you're looking for. Would you like me to broaden the search and provide related information that might be helpful?', don't try to make up an answer.
+prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, you should say that 'I've searched my database, but I couldn't locate the exact information you're looking for. Would you like me to broaden the search and provide related information that might be helpful?', don't try to make up an answer and don't return anything else.
 
 {context}
 
