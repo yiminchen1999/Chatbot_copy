365812d9f5efd7d103bbc167f2c0dff866396f73
Ciel (Xi) <122921783+CielZ001@users.noreply.github.com>
2023-07-19 16:52:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from langchain.callbacks.base import BaseCallbackHandler
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import ChatMessage
-import streamlit as st
+from streamlit_chat import message
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
 from langchain.vectorstores import Pinecone
@@ -9,7 +6,19 @@ from langchain.memory import ConversationSummaryBufferMemory
 from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain import PromptTemplate
+import openai
 import os
+import streamlit as st
+
+prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, you should say that 'I've searched my database, but I couldn't locate the exact information you're looking for. However, some of the documents did mention part of the keywords as listed. Would you like me to broaden the search and provide related information that might be helpful?', don't try to make up an answer.
+
+{context}
+
+Question: {question}
+Helpful Answer:"""
+QA_PROMPT_ERROR = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
 
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 pc_api_key = os.environ['pc_api_key']
@@ -22,11 +31,10 @@ embed = OpenAIEmbeddings(
     openai_api_key=OPENAI_API_KEY
 )
 
-pinecone.init(
-    api_key=pc_api_key,
-    environment=pc_env
-)
-
+pinecone.init(      
+	api_key = pc_api_key,      
+	environment = pc_env      
+)      
 index = pinecone.Index(pc_index)
 
 text_field = "text"
@@ -35,66 +43,13 @@ vectorstore = Pinecone(
     index, embed.embed_query, text_field
 )
 
-# Site title
-st.title("🤖🔬 ChatBot for Learning Sciences Research")
-
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [ChatMessage(role="assistant", content="How can I assist you?")]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg.role).write(msg.content)
-
-memory = ConversationSummaryBufferMemory(llm=OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY), max_token_limit=150,
-                                         memory_key='chat_history', return_messages=True, output_key='answer')
-
-
-if 'buffer_memory' not in st.session_state:
-    st.session_state.buffer_memory = memory
-
-
-CONDENSE_PROMPT = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:"""
-
-CONDENSEprompt = PromptTemplate(input_variables=["chat_history", "question"], template=CONDENSE_PROMPT)
-
-
-# If the question is not related to the context, politely respond that you are teached to only answer questions that are related to the context.
-
-QA_PROMPT_DOCUMENT_CHAT = """You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say 'I've searched my database, but I couldn't locate the exact information you're looking for. However, some of the documents did mention part of the keywords as listed. Would you like me to broaden the search and provide related information that might be helpful?'. DO NOT try to make up an answer.
-Answer in markdown.
-Use as much detail as possible when responding and try to make answer in markdown format as much as possible.
-
-{context}
-
-Question: {question}
-Answer in markdown format:"""
-
-
-QA_PROMPT_ERROR = PromptTemplate(
-    template=QA_PROMPT_DOCUMENT_CHAT, input_variables=["context", "question"]
-)
-
-
-class StreamHandler(BaseCallbackHandler):
-    def __init__(self, container, initial_text=""):
-        self.container = container
-        self.text = initial_text
-
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        self.text += token
-        self.container.markdown(self.text)
+memory = ConversationSummaryBufferMemory(llm=OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY), max_token_limit=150, memory_key='chat_history', return_messages=True, output_key='answer')
 
 
 def print_answer_citations_sources(result):
     output_answer = ""
 
-    # output_answer += result['answer'] + "\n\n"
+    output_answer += result['answer'] + "\n\n"
 
     unique_citations = {}
     for doc in result['source_documents']:
@@ -109,24 +64,78 @@ def print_answer_citations_sources(result):
 
     return output_answer
 
-# combine_docs_chain_kwargs={'prompt': QA_PROMPT_ERROR}
 
-if prompt := st.chat_input():
-    st.session_state.messages.append(ChatMessage(role="user", content=prompt))
-    st.chat_message("user").write(prompt)
+st.title("🤖🔬 ChatBot for Learning Sciences Research")
 
-    with st.chat_message("assistant"):
-        # response = llm(st.session_state.messages)
-        stream_handler = StreamHandler(st.empty())
-        # llm = ChatOpenAI(openai_api_key=openai_api_key, streaming=True, callbacks=[stream_handler])
-        qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, streaming=True, callbacks=[stream_handler]),
-                                                   vectorstore.as_retriever(), memory=st.session_state.buffer_memory,
-                                                   verbose=True,
-                                                   return_source_documents=True,
-                                                   condense_question_prompt = CONDENSEprompt,
-                                                   combine_docs_chain_kwargs={'prompt': QA_PROMPT_ERROR})
-        res = qa({"question": st.session_state.messages[-1].content})
-        response = print_answer_citations_sources(res)
-        st.write(response)
-        st.session_state.messages.append(ChatMessage(role="assistant", content=res['answer']))
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["How can I assist you?"]
 
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
+
+if 'buffer_memory' not in st.session_state:
+    st.session_state.buffer_memory = memory
+
+
+qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY),
+                                           vectorstore.as_retriever(), memory=st.session_state.buffer_memory,
+                                           verbose=True,
+                                           return_source_documents=True,
+					   combine_docs_chain_kwargs={'prompt': QA_PROMPT_ERROR})
+
+response_container = st.container()
+
+textcontainer = st.container()
+
+class MyThread(threading.Thread):
+    def __init__(self, func, args=()):
+        super(MyThread, self).__init__()
+        self.func = func
+        self.args = args
+    def run(self):
+        self.result = self.func(*self.args)
+    def get_result(self):
+        threading.Thread.join(self)
+        try:
+            return self.result
+        except Exception:
+            return None
+
+def st_spinner(t):
+    with st.spinner("Processing..."):
+        time.sleep(t)
+        st.write("Searching in the database...")
+        time.sleep(t)
+        st.write("Generating response...")
+        time.sleep(t)
+        st.write("Generating citation...")
+
+def get_res(query):
+    res = qa({"question": query})
+    response = print_answer_citations_sources(res)
+    return res
+
+with textcontainer:
+    query = st.text_input("Query: ", key="input")
+    if query:
+        t1 = MyThread(st_spinner,(5,))
+        t2 = MyThread(get_res,(query,))
+        
+        t1.start()
+        t2.start()
+        
+        t1.join()
+        t2.join()
+        
+        response = t1.get_result()
+	    # res = qa({"question": query})
+	    # response = print_answer_citations_sources(res)
+	    st.session_state.requests.append(query)
+	    st.session_state.responses.append(response)
+with response_container:
+    if st.session_state['responses']:
+
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i], key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
