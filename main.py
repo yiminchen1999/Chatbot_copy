88b36563ebc81eca988dbf775c9591bfba182fd1
Ciel (Xi) <122921783+CielZ001@users.noreply.github.com>
2023-07-20 13:35:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 import streamlit as st
 import time
 
-disableWidgetStateDuplicationWarning = True
+# disableWidgetStateDuplicationWarning = True
 
 prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, you should say that 'I've searched my database, but I couldn't locate the exact information you're looking for. However, some of the documents did mention part of the keywords as listed. Would you like me to broaden the search and provide related information that might be helpful?', don't try to make up an answer.
 
@@ -84,9 +84,21 @@ def extract_page_content_and_title(result):
     return extracted_string
 
 st.title("🤖🔬 ChatBot for Learning Sciences Research")
+st.sidebar.title('Pandas AI Agent 🤖 Demo')
+st.sidebar.write("""
+    ###### This project uses LangChain library utilizing OpenAI and Learning Sciences Research Database to act as a research assistant for researchers in related fields.
+    ###### We are a research team from TLT Lab, Columbia University.
+    ###### [Contact us](yz3204@tc.columbia.edu)
+    ###### [TLT Lab](https://tltlab.org/)
+    """)
 
 # response = ""
 
+st.sidebar.title('This response is from:')
+# dt = st.button('This response is from')
+# if response:
+	# if dt:
+	
 if 'responses' not in st.session_state:
     st.session_state['responses'] = ["How can I assist you?"]
 
@@ -119,6 +131,7 @@ with textcontainer:
 			st.write("Formatting response...")
 		res = qa({"question": query})
 		response = print_answer_citations_sources(res)
+		details = extract_page_content_and_title(res)
 		st.session_state.requests.append(query)
 		st.session_state.responses.append(response)
 
@@ -127,12 +140,10 @@ with response_container:
 	if st.session_state['responses']:
 		for i in range(len(st.session_state['responses'])):
 			message(st.session_state['responses'][i], key=str(i))
-			# dt = st.button('Click to see where is this response from')
-			# if dt:
-			# 	details = extract_page_content_and_title(res)
-			# 	st.write(details)
 			if i < len(st.session_state['requests']):
 				message(st.session_state["requests"][i], is_user=True, key=str(i) + '_user')
+		st.sidebar.write(details)
+
 
 		# count += 1
 		# try:
