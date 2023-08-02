7036865221a709e714758a57c40fdbbb5ab70045
Ciel (Xi) <122921783+CielZ001@users.noreply.github.com>
2023-08-01 22:45:42 -0400

    Update Chat_with_documents.py

--- pages/Chat_with_documents.py
+++ pages/Chat_with_documents.py
@@ -46,25 +46,26 @@ def is_openai_api_key_set() -> bool:
 def main():
     if len(st.session_state) == 0:
         st.session_state["messages"] = []
-        st.session_state["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "")
-        if is_openai_api_key_set():
-            st.session_state["agent"] = Agent(st.session_state["OPENAI_API_KEY"])
-        else:
-            st.session_state["agent"] = None
+        st.session_state["OPENAI_API_KEY"] = os.environ['OPENAI_API_KEY']
+        st.session_state["agent"] = Agent(st.session_state["OPENAI_API_KEY"])
+        # if is_openai_api_key_set():
+        #     st.session_state["agent"] = Agent(st.session_state["OPENAI_API_KEY"])
+        # else:
+        #     st.session_state["agent"] = None
 
     st.header("ChatPDF")
 
-    if st.text_input("OpenAI API Key", value=st.session_state["OPENAI_API_KEY"], key="input_OPENAI_API_KEY", type="password"):
-        if (
-            len(st.session_state["input_OPENAI_API_KEY"]) > 0
-            and st.session_state["input_OPENAI_API_KEY"] != st.session_state["OPENAI_API_KEY"]
-        ):
-            st.session_state["OPENAI_API_KEY"] = st.session_state["input_OPENAI_API_KEY"]
-            if st.session_state["agent"] is not None:
-                st.warning("Please, upload the files again.")
-            st.session_state["messages"] = []
-            st.session_state["user_input"] = ""
-            st.session_state["agent"] = Agent(st.session_state["OPENAI_API_KEY"])
+    # if st.text_input("OpenAI API Key", value=st.session_state["OPENAI_API_KEY"], key="input_OPENAI_API_KEY", type="password"):
+    #     if (
+    #         len(st.session_state["input_OPENAI_API_KEY"]) > 0
+    #         and st.session_state["input_OPENAI_API_KEY"] != st.session_state["OPENAI_API_KEY"]
+    #     ):
+    #         st.session_state["OPENAI_API_KEY"] = st.session_state["input_OPENAI_API_KEY"]
+    #         if st.session_state["agent"] is not None:
+    #             st.warning("Please, upload the files again.")
+    #         st.session_state["messages"] = []
+    #         st.session_state["user_input"] = ""
+    #         st.session_state["agent"] = Agent(st.session_state["OPENAI_API_KEY"])
 
     st.subheader("Upload a document")
     st.file_uploader(
@@ -80,10 +81,10 @@ def main():
     st.session_state["ingestion_spinner"] = st.empty()
 
     display_messages()
-    st.text_input("Message", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
+    # st.text_input("Message", key="user_input", disabled=not is_openai_api_key_set(), on_change=process_input)
 
-    st.divider()
-    st.markdown("Source code: [Github](https://github.com/viniciusarruda/chatpdf)")
+    # st.divider()
+    # st.markdown("Source code: [Github](https://github.com/viniciusarruda/chatpdf)")
 
 
 if __name__ == "__main__":
